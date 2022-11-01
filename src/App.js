9c78d6276426cd297e6b5d08d2a5ac2aa5ff135a
Tessaro <guilherme.tessaro27@gmail.com>
2022-11-01 15:09:05 -0300

    button to add posts

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import {Box, Button, Stack} from "@mui/material"
-import ManageHistoryIcon from '@mui/icons-material/ManageHistory';
+import {Box, createTheme, Stack} from "@mui/material"
 import Sidebar from "./components/Sidebar";
 import Feed from "./components/Feed";
 import Rightbar from "./components/Rightbar";
-import { Container } from "@mui/system";
 import Navbar from "./components/Navbar";
+import Add from "./components/Add";                  
+import { ThemeProvider } from "@emotion/react";
+import { useState } from "react";
 
 function App() {
+
+  const [mode, setMode] =useState("dark")
+
+  const darkTheme = createTheme({
+    palette:{
+      mode: mode
+    }
+  })
+
   return (
-    <Box>
-      <Navbar/>
-      <Stack direction="row" spacing={2} justifyContent="space-evenly">
-        <Sidebar/>
-        <Feed/>
-        <Rightbar/>
-      </Stack>
-    </Box>
+    <ThemeProvider theme={darkTheme}>
+      <Box color="text.secondary">
+        <Navbar/>
+        <Stack direction="row" spacing={2} justifyContent="space-evenly">
+          <Sidebar/>
+          <Feed/>
+          <Rightbar/>
+        </Stack>
+        <Add/>
+      </Box>
+    </ThemeProvider>
   );
 }
 
